f15aba9f06fa981aec5fb10a9e4f827a0ad2c05e
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-11-15 13:16:58 +0900

    fix: Patch options not reset in `ARSCLib` patch

--- wsEvents/resetPatchOptions.js
+++ wsEvents/resetPatchOptions.js
@@ -1,10 +1,14 @@
-const { existsSync } = require('node:fs');
+const { existsSync, rmSync } = require('node:fs');
 const exec = require('../utils/promisifiedExec.js');
 
 /**
  * @param {import('ws').WebSocket} ws
  */
 module.exports = async function resetPatchOptions(ws) {
+  if (existsSync('options.json')) {
+    rmSync('options.json', { recursive: true, force: true });
+  }
+
   const java = `${global.javaCmd}`;
   const cli = `${global.jarNames.cli}`;
   const patches = `${global.jarNames.patchesJar}`;
